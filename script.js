a064f7d517dbdff40a1b12bde61945ac6f8dec2d
ThanhTam712 <lethithanhtam071205@gmail.com>
2024-02-27 13:42:46 +0700

    init

---
+++ script.js
@@ -0,0 +1,405 @@
+const questions = [
+    {
+        question: '1. CVĐ xây dựng gia đình 5 không 3 sạch được TW Hội LHPN phát động thời gian nào?',
+        image: '',
+        image: '',
+        ans: [
+            'Năm 2008',
+            'Năm 2009',
+            'Năm 2010',
+            'Năm 2011'
+        ],
+        answer: 3
+    },
+    {
+        question: '2. Đâu là các tiêu chí của nội dung 5 không?',
+        image: '',
+        image: '',
+        ans: [
+            'Không đói nghèo, không VPPL, không vi phạm TNXH, không bạo lực, không vi phạm chính sách dân số',
+            'Không đói nghèo, không VPPL, không VP TNXH, không bạo lực, không có trẻ suy dinh dưỡng',
+            'Không đói nghèo, không VPPL, không bạo lực, không có trẻ suy dinh dưỡng, không có con bỏ học',
+            'Không đói nghèo, không VPPL và TNXH, không bạo lực, không vi phạm chính sách dân số, không có trẻ suy dinh dưỡng và bỏ học.'
+        ],
+        answer: 4
+    },
+    {
+        question: '3. Nội dung của tiêu chí 3 sạch đó là :',
+        image: '',
+        image: '',
+        ans: [
+            'Sạch nhà- sạch vườn – sạch ngõ',
+            'Sạch nhà – sạch bếp- sạch ngõ',
+            'Sạch nhà -sạch bếp- sạch vườn',
+            'Sạch bếp-sạch vườn-sạch ngõ'
+        ],
+        answer: 2
+    },
+    {
+        question: '4. Để gia đình không có bạo lực thì cần phải làm gì?',
+        image: '',
+        image: '',
+        ans: [
+            'Moị thành viên trong gia đình yêu thương, tôn trọng nhau',
+            'Các thành viên chia sẽ mọi quyền lợi và nghĩa vụ với nhau',
+            'Gia đình không có bạo lực về thể chất, tinh thần, kinh tế , tình dục.',
+            'Tất cá các đáp án trên'
+        ],
+        answer: 4
+    },
+    {
+        question: '5. Tiêu chí sạch nhà được hiểu là ?',
+        image: '',
+        image: '',
+        ans: [
+            'Nhà cửa luôn sạch sẽ, gọn gàng',
+            'Nhà cửa sạch sẽ, có nước giếng hoặc nước máy để sử dụng, có nhà tắm  nhà tiêu hợp vệ sinh, không nhốt gia súc gần nhà.',
+            'Nhà cửa sạch sẽ, có nước giếng hoặc nước máy sử dụng.',
+            'Nhà cửa sạch sẽ, ngăn nắp, không nhốt gia súc gần nhà và đảm bảo vệ sinh.'
+        ],
+        answer: 2
+    },
+    {
+        question: '6. Gia đình không có người VPPL và TNXH gồm  các tiêu chí nào?',
+        image: '',
+        image: '',
+        ans: [
+            'PN và các thành viên trong gia đihf thực hiện tốt các chủ trương, chính sách pháp luật của Đảng, Nhà nước',
+            'Không có người thân vi phạm PL bị xử lí hành chính hoặc truy tố',
+            'Không có thành viên tham gia khiếu kiện trái pháp luật.',
+            'Tất cả các nội dung trên'
+        ],
+        answer: 4
+    },
+    {
+        question: '7. Những việc PN Cần  làm để xây dựng gia đình 5 không 3 sạch?',
+        image: '',
+        image: '',
+        ans: [
+            'Sắp xếp nhà cửa gọn gàng, sạch đẹp, thường xuyên dọn vệ sinh nhà, bếp, ngõ. Động viên nam giới chia sẻ công việc gia đình. Học tập kiến thức khoa học kỹ thuật, học nghề, áp dụng vào sản xuất để phát triển kinh tế gia đình, tăng thu nhập.',
+            'Áp dụng kiến thức vệ sinh an toàn thực phẩm trong sản xuất, chế biến, kinh doanh. Học tập và áp dụng kiến thức nuôi, dạy con theo khoa học.',
+            'Chủ động cùng các thành viên tổ chức tốt cuộc sống gia đình, phòng ngừa bạo lực gia đình; phòng ngừa tệ nạn xã hội, tội phạm xâm nhập vào gia đình.',
+            'Tất cả các nội dung trên'
+        ],
+        answer: 4
+    },
+    {
+        question: '8. Đại hội đại biểu phụ nữ toàn quốc lần thứ XIII tiếp tục phát động cuộc vận động nào?',
+        image: '',
+        image: '',
+        ans: [
+            '“Xây dựng gia đình 5 có, 3 sạch”',
+            '“Xây dựng người Phụ nữ Việt Nam thời đại mới”',
+            '“Xây dựng gia đình 5 không, 3 sạch”',
+            'Cả 3 đáp án trên'
+        ],
+        answer: 3
+    },
+    {
+        question: '9. Đại hội đại biểu Phụ nữ tỉnh Quảng Trị lần thứ XV, nhiệm kỳ 2021-2026 đã thay đổi và điều chỉnh tên gọi của cuộc vận động “ 5 không 3 sạch” như thế nào?',
+        image: '',
+        image: '',
+        ans: [
+            'Xây dựng gia đình 5 không 3 sạch, góp phần xây dựng nông thôn mới và đô thị văn minh.',
+            'Xây dựng gia đình 5 không 3 sạch, góp phần xóa đói giảm nghèo.',
+            'Xây dựng gia đình 5 không 3 sạch, góp phần bảo vệ môi trường.',
+            'Xây dựng gia đình 5 không 3 sạch, góp phần vun đắp giá trị gia đình Việt Nam'
+        ],
+        answer: 4
+    },
+    {
+        question: '10. Hàng năm, mỗi cơ sở Hội vận động hỗ trợ ít nhất bao nhiêu hộ gia đình đạt tiêu chí “5 không, 3 sạch” ?',
+        image: '',
+        image: '',
+        ans: [
+            '5-7 hộ',
+            '6-7 hộ ',
+            '7-8 hộ',
+            '8-9 hộ'
+        ],
+        answer: 1
+    },
+    {
+        question: '11. Nội dung  Xây dựng mô hình “Gia đình 5 có, 3 sạch” là điểm mới của nghị quyết Đại Hội nào?',
+        image: '',
+        image: '',
+        ans: [
+            'Nghị quyết ĐHĐBPNTQ lần thứ XI, nhiệm kỳ 2012-2017',
+            'Nghị quyết ĐHĐBPNTQ lần thứ XII, nhiệm kỳ 2017-2022',
+            'Nghị quyết ĐHĐBPNTQ lần thứ XIII, nhiệm kỳ 2022-2027 ',
+            'Cả A,B,C đều sai'
+        ],
+        answer: 3
+    },
+    {
+        question: '12. Nội dung trọng tâm tiêu chí “5 có” của mô hình “Gia đình 5 có, 3 sạch” là:',
+        image: '',
+        image: '',
+        ans: [
+            'Có ngôi nhà an toàn, có sức khoẻ, có sinh kế bền vững, có kiến thức, có đạo đức',
+            'Có ngôi nhà an toàn, có sức khoẻ, có sinh kế bền vững, có kiến thức, có trách nhiệm',
+            'Có sức khoẻ, có sinh kế bền vững, có kiến thức, có trách nhiệm, có văn hoá',
+            'Có ngôi nhà an toàn, có sức khoẻ, có sinh kế bền vững, có kiến thức, có nếp sống văn hoá'
+        ],
+        answer: 4
+    },
+    {
+        question: '13. Tiêu chí có ngôi nhà an toàn được hiểu như thế nào?',
+        image: '',
+        image: '',
+        ans: [
+            'Nhà ở đảm bảo an toàn đạt tiêu chuẩn 3 cứng theo quy định (nền cứng, khung cứng, mái cứng, diện tích nhà ở đạt từ 14m2/người trở lên, các công trình bếp, nhà vệ sinh, chuồng trại chăn nuôi phải được bố trí đảm bảo vệ sinh, thuận tiện . Có khuôn viên, cảnh quan, môi trường xanh - sạch - đẹp theo quy hoạch vườn mẫu nông thôn mới.',
+            'Có môi trường sống an toàn để các cá nhân trong gia đình được sinh ra, nuôi dưỡng và trưởng thành .Có phương tiện, điều kiện cơ sở vật chất gia đình dự phòng chủ động phòng chống tai nạn thương tích cho trẻ em.',
+            'Các thành viên trong gia đình được yêu thương, chia sẻ, bình đẳng, tôn trọng, gắn kết và đảm bảo an toàn về tính mạng, thân thể, tinh thần, sức khỏe, an toàn trong gia đình và trong không gian mạng; không bạo lực, xâm hại về thể chất, tinh thần, kinh tế.',
+            'Tất cả các nội dung trên'
+        ],
+        answer: 4
+    },
+    {
+        question: '14. Ý nghĩa của việc xây dựng mô hình 5 có 3 sạch ?',
+        image: '',
+        image: '',
+        ans: [
+            'Việc xây dựng mô hình là cách làm sáng tạo, thiết thực nhằm phát huy vai trò, trách nhiệm của hội viên, phụ nữ các hộ gia đình và cán bộ hội các cấp trong thực hiện Cuộc vận động “Toàn dân đoàn kết xây dựng NTM, đô thị văn minh”.',
+            'Quá trình thực hiện mô hình góp phần giúp hội viên, phụ nữ các hộ gia đình, người dân và cộng đồng có điều kiện học tập, trao đổi kinh nghiệm, liên kết trong hoạt động sản xuất, phát triển kinh tế, bảo vệ môi trường nông thôn ngày càng sáng-xanh-sạch-đẹp.',
+            'Cả A và B đều đúng.',
+            'Cả A và B đều sai'
+        ],
+        answer: 3
+    },
+    {
+        question: '15. Để  thành lập mô hình “Gia đình 5 có, 3 sạch” cần thực hiện tốt các khâu nào sau đây ?',
+        image: '',
+        image: '',
+        ans: [
+            'Rà soát, đánh giá thực trạng, bàn giải pháp hỗ trợ, giúp đỡ các hộ gia đình theo 8 tiêu chí “Gia đình 5 có, 3 sạch” phù hợp với tình hình thưc tế của địa phương.',
+            'Tổ chức hội nghị triển khai thí điểm mô hình; tập huấn, truyền thông kiến thức về các tiêu chí “Gia đình 5 có, 3 sạch”, các nội dung cần thực hiện, các chỉ tiêu phấn đấu, biểu mẫu theo dõi.',
+            'Biểu dương các tập thể, cá nhân và gia đình tiêu biểu trong thực hiện mô hình. Sơ kết, đánh giá mô hình gắn với kết quả thực hiện cuộc vận động “Xây dựng gia đình 5 không, 3 sạch” lồng ghép trong báo cáo sơ, tổng kết công tác Hội và phong trào phụ nữ.',
+            'Tất cả các nội dung trên'
+        ],
+        answer: 4
+    },
+    {
+        question: '16. Trường hợp nào sau đây sinh con không bị coi là vi phạm chính sách dân số và KHHGĐ?',
+        image: '',
+        image: '',
+        ans: [
+            'cặp vc sinh lần thứ nhất mà sinh ba ',
+            'cặp vc đã có 1 con, sinh lần thứ 2 sinh đôi',
+            'cặp vc sinh con t3 nếu đã có 2 con  đẻ nhưng 1 hoặc cả 2 con bị dị tật hoặc bệnh hiểm nghèo đã được xác nhận',
+            'tất cả các trường hợp trên đều đúng'
+        ],
+        answer: 4
+    },
+    {
+        question: '17. Theo chính sách dân số,kế hoạch hóa gđ độ tuổi để simh con phù hợp là: ',
+        image: '',
+        image: '',
+        ans: [
+            '22-35',
+            '20-30',
+            '25-35',
+            '20-35'
+        ],
+        answer: 4
+    },
+    {
+        question: '18. Theo pháp lệnh dân số năm 2003 hành vi nào sau đây bị nghiêm cấm:',
+        image: '',
+        image: '',
+        ans: [
+            'Cản trở, cưỡng bức việc thực hiện kế hoạch hóa gia đình',
+            'Lựa chọn giới tính thai nhi dưới mọi hình thức',
+            'Tuyên truyền, phổ biến thông tin sai với chính sách dân số, truyền thống tốt đẹp của dân tộc ',
+            'Tất cả các đáp án trên đều đúng.'
+        ],
+        answer: 4
+    },
+    {
+        question: '19. Gia đình không có trẻ suy dinh dưỡng và bỏ học là: ',
+        image: '',
+        image: '',
+
+        
+        ans: [
+            'Việc xây dựng mô hình là cách làm sáng tạo, thiết thực nhằm phát huy vai trò, trách nhiệm của hội viên, phụ nữ các hộ gia đình và cán bộ hội các cấp trong thực hiện Cuộc vận động “Toàn dân đoàn kết xây dựng NTM, đô thị văn minh”.',
+            'Quá trình thực hiện mô hình góp phần giúp hội viên, phụ nữ các hộ gia đình, người dân và cộng đồng có điều kiện học tập, trao đổi kinh nghiệm, liên kết trong hoạt động sản xuất, phát triển kinh tế, bảo vệ môi trường nông thôn ngày càng sáng-xanh-sạch-đẹp.',
+            'Cả A và B đều đúng.',
+            'Cả A và B đều sai'
+        ],
+        answer: 1
+    },
+    {
+        question: '20. Khoảng cách sinh con tốt nhất cho phụ nữ là bao nhiêu năm?',
+        image: '',
+        image: '',
+        ans: [
+            '1-3 năm',
+            '2-4 năm',
+            '3-5 năm',
+            '4-6 năm'
+        ],
+        answer: 3
+    }
+]
+
+const startScreen = document.querySelector(".start");
+const informationScreen = document.querySelector(".information");
+const quizScreen = document.querySelector(".content");
+const backBox = document.querySelector('.back');
+const resultBox = document.querySelector('.result-box');
+const goHomeBtn = document.querySelector('.goHome-btn');
+const pointScreen = document.querySelector(".ending");
+
+const continueElement = document.querySelector("#continue");
+continueElement.addEventListener("click", () => {
+    startScreen.hidden = true;
+    informationScreen.hidden = false;
+});
+
+const formElement = document.querySelector("#user_information");
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const unameValue = document.getElementById('input_user_name').value;
+    const uidValue = document.getElementById('input_user_id').value;
+    const uclassValue = document.getElementById('input_user_class').value;
+    const umajorValue = document.getElementById('input_user_major').value;
+    informationScreen.hidden = true;
+    quizScreen.hidden = false;
+    quiz();
+});
+
+let score = 0;
+let time = 0;
+let cans = -1;
+
+const quiz = () => {
+
+    const questionElement = document.querySelector("#question-text");
+    const answer1Element = document.querySelector("#ans_1");
+    const answer2Element = document.querySelector("#ans_2");
+    const answer3Element = document.querySelector("#ans_3");
+    const answer4Element = document.querySelector("#ans_4");
+    const timeElement = document.querySelector("#time");
+
+    const startTime = (Date.now() / 1000) | 0;
+    
+    const displayTime = () => {
+        const currentTime = (Date.now() / 1000) | 0;
+        const secondsElapsed = currentTime - startTime;
+        time = secondsElapsed;
+        const seconds = secondsElapsed % 60;
+        const minutes = (secondsElapsed / 60) | 0;
+        if (minutes > 0)
+            timeElement.textContent = `${minutes} min ${seconds} sec`;
+        else 
+            timeElement.textContent =  `${seconds} sec`;
+    };
+
+    const interval = setInterval(displayTime, 1000);
+    displayTime();
+
+    let questionNumber = 0;
+
+    const handleAnswer = (selectedAnswerElement, selectedAnswerIndex) => {
+        // Kiểm tra xem đã chọn đáp án chưa
+        const question = questions[questionNumber];
+        if (!selectedAnswerElement.classList.contains('selected')) {
+            selectedAnswerElement.classList.add('selected');
+
+            const correctAnswerIndex = question.answer - 1;
+            if (selectedAnswerIndex === correctAnswerIndex) {
+                selectedAnswerElement.classList.add('correct');
+                score++;
+            } else {
+                selectedAnswerElement.classList.add('incorrect');
+                const correctAnswerElement = document.getElementById(`ans_${question.answer}`);
+                correctAnswerElement.classList.add('correct');
+            }
+    
+            setTimeout(nextQuestion, 300); // Chuyển câu hỏi sau 0.15 giây
+    
+        }
+    };
+    
+    const displayQuestion = (question) => {
+        const allAnswerElements = document.querySelectorAll('.option');
+        allAnswerElements.forEach(answerElement => {
+            answerElement.classList.remove('selected', 'correct', 'incorrect');
+        });
+
+        question.ans.forEach((answer, index) => {
+            const answerElement = document.getElementById(`ans_${index + 1}`);
+            answerElement.textContent = String.fromCharCode(65 + index) + '. ' + answer;
+        });
+
+        isAnswered = false;
+
+        questionElement.textContent = question.question;
+
+        answer1Element.textContent = "A. " + question.ans[0];
+        answer2Element.textContent = "B. " + question.ans[1];
+        answer3Element.textContent = "C. " + question.ans[2];
+        answer4Element.textContent = "D. " + question.ans[3];
+        
+        cans = question.answer;
+        console.log(cans);
+
+        answer1Element.addEventListener("click", () => handleAnswer(answer1Element, 0));
+        answer2Element.addEventListener("click", () => handleAnswer(answer2Element, 1));
+        answer3Element.addEventListener("click", () => handleAnswer(answer3Element, 2));
+        answer4Element.addEventListener("click", () => handleAnswer(answer4Element, 3));
+    };
+
+    const nextQuestion = () => {
+        const allAnswerElements = document.querySelectorAll('.option');
+        allAnswerElements.forEach(answerElement => {
+            answerElement.classList.remove('selected', 'correct', 'incorrect');
+        });
+        questionNumber++;
+        if (questionNumber === questions.length) {
+            setTimeout(() => {
+                clearInterval(interval);
+                quizScreen.hidden = true;
+                pointScreen.hidden = false;
+                // Set text content along with the button using innerHTML
+                pointScreen.innerHTML = `
+                    <div>
+                        <p>Số câu đúng: ${score}/${questions.length}. Cám ơn hội viên đã tham gia!</p>
+                        <a href="index.html" class="buttons"><h4 class="goHome-btn">Quay lại trang chủ</h4></a>
+                    </div>`;
+                goHomeBtn.onclick = () => {
+                    quizScreen.classList.remove('active'); // Assuming this is the correct class name to remove
+                    resultBox.classList.remove('active');
+                    questionCount = 0;
+                    questionNumb = 1;
+                    userScore = 0;
+                    showQuestions(questionCount);
+                    questionCounter(questionNumb);
+                }
+                sendData();
+            }, 200);
+            return;
+        }
+        displayQuestion(questions[questionNumber]);
+    };
+    
+    displayQuestion(questions[0]);
+    
+    const sendData = () => { 
+        const urlObject = new URL("https://script.google.com/macros/s/AKfycbyqs1upJ111FDObhzGsvhxz0oxNQSAIfbN_e0zsWNebsYTSg79RKAc5HBhFyE5GR7s49A/exec");
+        
+        const unameValue = document.getElementById('input_user_name').value;
+        const uidValue = document.getElementById('input_user_id').value;
+        const uclassValue = document.getElementById('input_user_class').value;
+        const umajorValue = document.getElementById('input_user_major').value;
+
+        urlObject.searchParams.set("uname", unameValue);
+        urlObject.searchParams.set("uid", uidValue);
+        urlObject.searchParams.set("uclass", uclassValue);
+        urlObject.searchParams.set("umajor", umajorValue);
+        urlObject.searchParams.set("uscore", score);
+
+        fetch(urlObject.toString());
+    };
+};
